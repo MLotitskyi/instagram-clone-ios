340f6ad712ee1845f04980a389e596aaa487b845
Maksym <maksimlotickij@gmail.com>
2025-12-14 00:01:38 +0100

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Максим Лотіцький on 14/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
