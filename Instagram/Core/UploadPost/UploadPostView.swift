633e143091006a31e701dcc357f3f5c93d7e8205
Maksym <maksimlotickij@gmail.com>
2026-01-11 20:22:28 +0100

    Add simple ImagePicker

---
+++ Instagram/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  Instagram
+//
+//  Created by Максим Лотіцький on 11/01/2026.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
---
+++ Instagram/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UploadPostViewModel.swift
+//  Instagram
+//
+//  Created by Максим Лотіцький on 11/01/2026.
+//
+
+import Foundation
